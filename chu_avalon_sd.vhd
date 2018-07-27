3cdd908203f696797dfc38c13a14ae085a6202f4
hakatu <hungnguyen110697@gmail.com>
2018-07-27 08:22:33 +0700

    Add files via upload

---
+++ chu_avalon_sd.vhd
@@ -0,0 +1,58 @@
+library ieee ; 
+use ieee.std_logic_1164. all; 
+use ieee.numeric_std.all; 
+entity chu_avalon_sd is 
+port ( 
+clk, reset_n: in std_logic;  
+--ext
+sd_cs, sd_clk, sd_di: out std_logic; 
+sd_do: in std_logic;
+--avl 
+sd_address: in std_logic_vector(1 downto 0); 
+sd_chipselect: in std_logic; 
+sd_write : in std_logic ; 
+sd_writedata: in std_logic_vector(31 downto 0); 
+sd_read: in std_logic; 
+sd_readdata: out std_logic_vector(31 downto 0) 
+); 
+end chu_avalon_sd; 
+architecture arch of chu_avalon_sd is 
+signal wr_en, wr_sd, wr_cs, wr_dvsr: std_logic; 
+signal cs_reg: std_logic; 
+signal dvsr_reg: std_logic_vector(7 downto 0); 
+signal sd_out: std_logic_vector(7 downto 0); 
+signal sd_ready: std_logic; 
+begin 
+--instantiate SPI unit 
+spi_unit : entity work.spi 
+port map(clk=>clk, reset_n=>reset_n,
+din=>sd_writedata(7 downto 0), 
+dvsr=>dvsr_reg, dout=>sd_out, wr_sd=>wr_sd, 
+spi_clk=>sd_clk,
+spi_mosi=>sd_di, spi_miso=>sd_do, 
+spi_idle=>sd_ready, spi_done_tick=>open); 
+--registers 
+process (clk, reset_n) 
+begin 
+if (reset_n='1') then 
+cs_reg <= '0'; 
+dvsr_reg <= (others=>'1'); 
+elsif (clk'event and clk='1') then 
+if (wr_cs='1') then 
+cs_reg <=sd_writedata(0); 
+end if; 
+if wr_dvsr = '1' then 
+dvsr_reg <=sd_writedata(7 downto 0); 
+end if; 
+end if; 
+end process ; 
+--write decoding 
+wr_en <= '1' when sd_write='1' and sd_chipselect='1' else '0'; 
+wr_cs <= '1' when sd_address="01" and wr_en= '1' else '0'; 
+wr_dvsr <= '1' when sd_address="10" and wr_en= '1' else'0' ; 
+wr_sd <= '1' when sd_address="11" and wr_en= '1' else '0'; 
+--read data 
+sd_readdata <= x"00000" & "000" & sd_ready & sd_out ;
+--output 
+sd_cs <= cs_reg; 
+end arch; 
---
+++ spi.vhd
@@ -0,0 +1,90 @@
+library ieee ; 
+use  ieee.std_logic_1164.all ; 
+use  ieee.numeric_std.all ; 
+entity spi is
+port ( 
+clk,  reset_n: in  std_logic ; 
+din: in std_logic_vector(7 downto 0); 
+dvsr: in std_logic_vector(7 downto 0); 
+wr_sd: in  std_logic ; 
+dout: out std_logic_vector(7 downto 0); 
+spi_clk: out std_logic; 
+spi_miso: in std_logic; 
+spi_mosi: out std_logic; 
+spi_done_tick, spi_idle: out std_logic 
+); 
+end  spi; 
+architecture arch of spi is 
+type  statetype is  ( idle, sclk0,  sclkl ); 
+signal  state_reg,  state_next:  statetype; 
+signal c_reg, c_next: unsigned(7 downto 0); 
+signal  spi_tick :  std_logic; 
+signal spi_clk_reg, spi_clk_next:  std_logic; 
+signal bit_reg ,  bit_next : unsigned(2 downto 0); 
+signal sin_reg,  sin_next : std_logic_vector(7 downto 0); 
+signal sout_reg, sout_next: std_logic_vector(7 downto 0); 
+begin 
+process (clk,  reset_n ) 
+begin 
+if  (reset_n = '1') then 
+state_reg <= idle; 
+sin_reg <= (others=>'0'); 
+sout_reg <= (others=>'0'); 
+bit_reg <= (others=>'0'); 
+c_reg <=(others=>'0'); 
+spi_clk_reg <= '0'; 
+elsif (clk'event and clk='1') then 
+state_reg <= state_next; 
+sin_reg <= sin_next; 
+sout_reg <= sout_next; 
+bit_reg <= bit_next; 
+c_reg <= c_next; 
+spi_clk_reg <= spi_clk_next;
+end if; 
+end process; 
+-- state machine
+process(state_reg,sin_reg,sout_reg,bit_reg ,din,wr_sd,spi_miso) 
+begin 
+state_next <= state_reg; 
+spi_idle <= '0'; 
+spi_done_tick <='0'; 
+sin_next <= sin_reg; 
+sout_next <= sout_reg; 
+bit_next <= bit_reg; 
+c_next <= c_reg + 1; -- chay timer
+case state_reg is 
+when idle => 
+spi_idle <= '1'; 
+if  wr_sd='1' then 
+sout_next <= din; 
+state_next <= sclk0; 
+bit_next <= (others=>'0'); 
+c_next <= x"01"; 
+end if; 
+when sclk0 => 
+if c_reg=unsigned(dvsr) then --spi-clk 0�to�1 
+state_next <= sclkl; 
+sin_next <= sin_reg(6 downto 0) & spi_miso; 
+c_next <= x"01"; 
+end if; 
+when sclkl => 
+if c_reg=unsigned(dvsr) then -- spi-clk l�to�0 
+if bit_reg=7 then 
+spi_done_tick <= '1' ; 
+state_next <= idle; 
+else 
+sout_next <= sout_reg(6 downto 0) & '0'; 
+state_next <= sclk0; 
+bit_next <= bit_reg + 1; 
+c_next <= x"01"; 
+end if; 
+end if; 
+end case; 
+end process ; 
+--lookahead output decoding 
+spi_clk_next <=  '1' when state_next=sclkl else '0'; 
+--output 
+dout <= sin_reg; 
+spi_mosi <= sout_reg(7); 
+spi_clk <= spi_clk_reg; 
+end arch;
